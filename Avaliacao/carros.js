fd27938ce3de94b7ec6980c0683446d17642e445
Luiz-AGM <luiz_agm@hotmail.com>
2022-03-30 10:08:58 -0300

    att avaliacao

--- Avaliacao/carros.js
+++ Avaliacao/carros.js
@@ -4,12 +4,13 @@ console.log("=== CARROS ===");
 1 - FAZER UMA COPIA DO EXERCICIO DA LOJA DE CARROS
 2 - ADICIONAR UMA COLUNA COM UM ID (AO LADO DO MODELO)
 3 - ADICIONAR NO FORMULARIO UM CAMPO PARA RECEBER O PRECO
-4 - EXIBIR UMA COLUNA COM O O PRECO
+4 - EXIBIR UMA COLUNA COM O PRECO
 5 - EXIBIR O TOTAL DE TODOS OS VEICULOS CADASTRADOS (REQUISITO -> USAR REDUCE)
 */
 
 const carros = [];
-const valores = []
+const valores = [];
+
 function adicionar() {
   // entrada
   const modelo = document.getElementById("modelo");
@@ -28,16 +29,30 @@ function adicionar() {
     cor: cor.value,
     situacao: situacao.value,
     preco: parseFloat(preco.value)
-  };
+    };
 
-  console.log(carro);
+    if (document.getElementById("preco").value == "" || 
+      document.getElementById("modelo").value == "" || 
+      document.getElementById("ano").value == "") {
+      alert('Por favor, preencha todos os campos');
+      // document.getElementById("preco").focus();
+      return false }
+
+    if (document.getElementById("preco").value < 0 || 
+      document.getElementById("ano").value <0 ) {
+      alert('Por favor, preencha com números positivos');
+      // document.getElementById("preco").focus();
+      return false }
+
+
   carros.push(carro);
-  valores.push(carro.preco)
-  console.log(valores)
+  valores.push(carro.preco);
+  console.log(carro);
+  console.log(valores);
 
   // saida
   console.table(carros);
-
+    
   // exibir a tabela
   const lista = document.getElementById("lista");
   lista.innerHTML = 
@@ -54,10 +69,11 @@ function adicionar() {
     })
     .join("");
 
- 
-  let total = valores.reduce(function(valor1, valor2) {
+  let total = 
+    valores
+      .reduce(function(valor1, valor2) {
     return valor1 + valor2;
-}, );
+  }, );
 
   console.log(total)
 
@@ -71,6 +87,7 @@ function adicionar() {
   preco.value = '';
 
   modelo.focus();
+  
 }
 
 
